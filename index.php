8fe3ef93be37f91114ad3d1a8597c5454363f92a
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-02-10 01:15:11 +0545

    add functions, screenshot, assets and templates.

--- index.php
+++ index.php
@@ -12,10 +12,10 @@
 <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>nexusolution </title>
+      <title> Nexusolution Wordpress Themes </title>
 </head>
 <body>
-      <h1>nexusolution</h1>
+      <h1>Nexusolution Wordpress Themes </h1>
 </body>
 </html>
 
