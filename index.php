0c9beb646f7d972dab50d59835bc90032dca8a9d
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-02-17 23:01:11 +0545

    basic layout...

--- index.php
+++ index.php
@@ -1,15 +1,36 @@
 <?php
 /**
- * Main Template 
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package Mamata
  */
 
+
+
 get_header();
+
 ?>
 
-<div class="content">content </div>
-      
-<?php
+<div id="content" class="site-content <?php echo esc_attr($xe_opt->container); ?> padding-top-bottom clearfix">
 
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+
+
+
+        </main><!-- #main -->
+    </div><!-- #primary -->
+
+    <?php get_sidebar(); ?>
+
+</div><!-- #content -->
+
+<?php
 get_footer();
